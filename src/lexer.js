603b328cf33e9ac8b5dd332cdfd1dd878c27a622
Richard Lau <rlau@redhat.com>
2020-10-13 18:28:37 -0400

    Replace endian specific copy functions with DataView

--- src/lexer.js
+++ src/lexer.js
@@ -2,21 +2,20 @@ const strictReserved = new Set(['implements', 'interface', 'let', 'package', 'pr
 
 let wasm;
 
-const isLE = new Uint8Array(new Uint16Array([1]).buffer)[0] === 1;
-
 export function parse (source, name = '@') {
   if (!wasm)
     throw new Error('Not initialized');
 
   const len = source.length + 1;
+  const byteLength = len * 2;
 
   // need 2 bytes per code point plus analysis space so we double again
   const extraMem = (wasm.__heap_base.value || wasm.__heap_base) + len * 4 - wasm.memory.buffer.byteLength;
   if (extraMem > 0)
     wasm.memory.grow(Math.ceil(extraMem / 65536));
     
-  const addr = wasm.sa(len);
-  (isLE ? copyLE : copyBE)(source, new Uint16Array(wasm.memory.buffer, addr, len));
+  const addr = wasm.sa(byteLength);
+  copy(source, new DataView(wasm.memory.buffer, addr, byteLength));
 
   if (!wasm.parseCJS(addr, source.length, 0, 0))
     throw Object.assign(new Error(`Parse error ${name}${wasm.e()}:${source.slice(0, wasm.e()).split('\n').length}:${wasm.e() - source.lastIndexOf('\n', wasm.e() - 1)}`), { idx: wasm.e() });
@@ -33,20 +32,11 @@ export function parse (source, name = '@') {
   return { exports: [...exports], reexports: [...reexports] };
 }
 
-function copyBE (src, outBuf16) {
-  const len = src.length;
-  let i = 0;
-  while (i < len) {
-    const ch = src.charCodeAt(i);
-    outBuf16[i++] = (ch & 0xff) << 8 | ch >>> 8;
-  }
-}
-
-function copyLE (src, outBuf16) {
+function copy (src, outBuf16) {
   const len = src.length;
   let i = 0;
   while (i < len)
-    outBuf16[i] = src.charCodeAt(i++);
+    outBuf16.setUint16(i * 2, src.charCodeAt(i++), true);
 }
 
 let initPromise;
