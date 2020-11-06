c310a816ae0c6b15a4df6f53fbc4d1f88b67cb2f
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2020-11-06 15:49:50 +0100

    fix(TypeScript): Expose `Exports` interface type (#30)

--- lexer.d.ts
+++ lexer.d.ts
@@ -1,9 +1,7 @@
-interface Exports {
+export interface Exports {
   exports: string[];
   reexports: string[];
 }
 
 export declare function parse(source: string, name?: string): Exports;
 export declare function init(): Promise<void>;
-
-export {};
