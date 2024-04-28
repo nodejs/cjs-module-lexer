8b807a5a405b5eadc596825e9ace1a88c3e0fb23
Guy Bedford <guybedford@gmail.com>
2024-04-28 11:13:12 -0700

    feat: support external wasm binary

--- build.js
+++ build.js
@@ -1,25 +1,33 @@
 const fs = require('fs');
 const terser = require('terser');
 
-const MINIFY = true;
-
-try { fs.mkdirSync('./dist'); }
-catch (e) {}
-
-const wasmBuffer = fs.readFileSync('./lib/lexer.wasm');
-const jsSource = fs.readFileSync('./src/lexer.js').toString();
-const pjson = JSON.parse(fs.readFileSync('./package.json').toString());
-
-const jsSourceProcessed = jsSource.replace('WASM_BINARY', wasmBuffer.toString('base64'));
-
-const minified = MINIFY && terser.minify(jsSourceProcessed, {
-  module: true,
-  output: {
-    preamble: `/* cjs-module-lexer ${pjson.version} */`
+function buildCjsModuleLexer(filename, inline, minify) {
+  try { fs.mkdirSync('./dist'); }
+  catch (e) {}
+  
+  const wasmBuffer = fs.readFileSync('./lib/lexer.wasm');
+  const jsSource = fs.readFileSync('./src/lexer.js').toString();
+  const pjson = JSON.parse(fs.readFileSync('./package.json').toString());
+  
+  let jsSourceProcessed;
+  if (inline) {
+    jsSourceProcessed = jsSource.replace('WASM_BINARY', `(binary => typeof window !== 'undefined' && typeof atob === 'function' ? Uint8Array.from(atob(binary), x => x.charCodeAt(0)) : Buffer.from(binary, 'base64'))("${wasmBuffer.toString('base64')}")`);
+  } else {
+    jsSourceProcessed = jsSource.replace('WASM_BINARY', `(await import('node:fs')).readFileSync(new URL(import.meta.resolve('../lib/lexer.wasm')))`);
   }
-});
 
-if (minified.error)
-  throw minified.error;
+  const minified = minify && terser.minify(jsSourceProcessed, {
+    module: true,
+    output: {
+      preamble: `/* cjs-module-lexer ${pjson.version} */`
+    }
+  });
+  
+  if (minified.error)
+    throw minified.error;
+  
+  fs.writeFileSync(`./dist/${filename}.mjs`, minified ? minified.code : jsSourceProcessed);
+}
 
-fs.writeFileSync('./dist/lexer.mjs', minified ? minified.code : jsSourceProcessed);
+buildCjsModuleLexer('lexer', true, true);
+buildCjsModuleLexer('lexer-external', false, false);
